e8f02dc98cf1f591e8a1f907bf43bde557decfeb
nafisholeh-apv <rachmad.nafisholeh@appvantage.asia>
2025-01-11 19:48:12 +0700

    feat: set up Express server with basic middleware

---
+++ src/controllers/userController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+export class UserController {
+  async create(req: Request, res: Response) {}
+
+  async delete(req: Request, res: Response) {}
+
+  async update(req: Request, res: Response) {}
+}
